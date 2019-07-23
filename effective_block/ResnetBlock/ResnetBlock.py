5754e7dc2ad1eaf99307c7ebde7425986107c57f
idotc <244979481@qq.com>
2019-07-23 23:03:02 +0800

    Update ResnetBlock.py

--- effective_block/ResnetBlock/ResnetBlock.py
+++ effective_block/ResnetBlock/ResnetBlock.py
@@ -4,25 +4,28 @@ import torch.nn as nn
 class ResnetBlock(nn.Module):
     '''Resnet block '''
     def __init__(self, inplanes, outplanes, stride=1):
-        super(HgResBlock, self).__init__()
-        self.inplanes = inplanes
-        self.outplanes = outplanes
+        super(ResnetBlock, self).__init__()
         midplanes = outplanes // 2
-        expand_ratio=2;
+        self.conv1 = nn.Conv2d(inplanes, midplanes, 1, 1, 0, bias=False)
+        self.bn1 = nn.BatchNorm2d(midplanes)
+        self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(inplanes , inplanes , 3, stride, 1, groups=inplanes , bias=False)
-        self.bn2 = nn.BatchNorm2d(inplanes )
-        self.relu2 = nn.ReLU6(inplace=True)
+        self.conv2 = nn.Conv2d(midplanes , midplanes , 3, stride, 1, groups=midplanes , bias=False)
+        self.bn2 = nn.BatchNorm2d(midplanes)
+        self.relu2 = nn.ReLU(inplace=True)
 
-        self.conv3 = nn.Conv2d(inplanes , outplanes, 1, 1, 0, bias=False)  # bias=False
+        self.conv3 = nn.Conv2d(midplanes , outplanes, 1, 1, 0, bias=False)  # bias=False
         self.bn3 = nn.BatchNorm2d(outplanes)
-        self.relu3= nn.ReLU6(inplace=True)
+        self.relu3= nn.ReLU(inplace=True)
         if inplanes != outplanes:
             self.conv_skip = nn.Conv2d(inplanes, outplanes, 1, 1)
 
     def forward(self, x):
         residual = x
-        out = self.conv2(x)
+	out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu2(out)
         out = self.conv3(out)
