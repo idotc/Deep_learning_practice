b333c8ee7a536b9698e0102522256c9665502021
idotc <244979481@qq.com>
2019-08-13 21:33:40 +0800

    Create fire_block.py

---
+++ effective_block/SqueezeBlock/fire_block.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+class FireBlock(nn.Module):
+
+    def __init__(self, in_channel, out_channel, squzee_channel):
+
+        super().__init__()
+        self.squeeze = nn.Sequential(
+            nn.Conv2d(in_channel, squzee_channel, 1),
+            nn.BatchNorm2d(squzee_channel),
+            nn.ReLU(inplace=True)
+        )
+
+        self.expand_1x1 = nn.Sequential(
+            nn.Conv2d(squzee_channel, int(out_channel / 2), 1),
+            nn.BatchNorm2d(int(out_channel / 2)),
+            nn.ReLU(inplace=True)
+        )
+
+        self.expand_3x3 = nn.Sequential(
+            nn.Conv2d(squzee_channel, int(out_channel / 2), 3, padding=1),
+            nn.BatchNorm2d(int(out_channel / 2)),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+
+        x = self.squeeze(x)
+        x = torch.cat([
+            self.expand_1x1(x),
+            self.expand_3x3(x)
+        ], 1)
+
+        return x
